adff27a674e7e01f749742d36d125378b80722ab
MIguel Torres <mtorres913@gmail.com>
2023-01-25 19:57:16 -0600

    created listIteams function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket =  []
+
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+
+//testing addItem
+console.log(addItem('grape'))
+
+function listItems(){
+    for (let item of basket){
+        console.log(item);
+    }
+}
+
+//testing listItems
+addItem('apple');
+listItems();
+addItem('bread');
+console.log (basket)
