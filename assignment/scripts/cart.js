ee1625789b8e131df3ed967ac72c3f9ebf2c8d09
MIguel Torres <mtorres913@gmail.com>
2023-01-31 16:03:46 -0600

    submiting assignment-unit-4.33

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,18 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket =  []
+let basket = [];
+const maxItems = 5
 
 function addItem(item){
     basket.push(item);
     return true;
 }
+addItem('grape');
 
 //testing addItem
-console.log(addItem('grape'))
+console.log(addItem('grape'));
+console.log(basket);
 
 function listItems(){
     for (let item of basket){
@@ -29,6 +32,34 @@ function empty(){
         basket.pop();
     }
 }
+//basket = []; also works as substitue of while loop
 //testing empty function
 empty()
 console.log(basket)
+
+
+function isFull(){
+     if (basket.length < maxItems){
+    return false;
+    } else {
+        return true;
+     }
+}
+//testing isFull function
+console.log(isFull())
+
+// function addItem(item){
+//     if (isFull()){
+//     basket.push(item);
+//     return true;
+//     } else {
+//         return false;
+//     }
+   
+// }
+console.log(basket)
+console.log(addItem('beans'))
+console.log(addItem('lightsaber'))
+console.log(addItem('pie'))
+console.log(basket)
+console.log(basket.length)
